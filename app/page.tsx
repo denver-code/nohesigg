c553d3fef7bcc46b88844ab6eab3c03cc1dc3c12
Ihor Savenko <csigorek@gmail.com>
2025-03-06 23:08:37 +0000

    add footer

--- app/page.tsx
+++ app/page.tsx
@@ -15,7 +15,7 @@ export default function Home() {
       {/* <ServersSection /> */}
       {/* <PlansSection /> */}
       {/* <ContactSection /> */}
-      {/* <Footer /> */}
+      <Footer />
     </div>
   )
 }
