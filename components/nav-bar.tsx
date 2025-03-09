aead6d50d64f91ffdd573cd6203a7435a45017d5
Ihor Savenko <csigorek@gmail.com>
2025-03-09 18:23:48 +0000

    updated navbar

--- components/nav-bar.tsx
+++ components/nav-bar.tsx
@@ -1,3 +1,4 @@
+"use client";
 import Link from "next/link"
 import { Bell, Star , Server, Crown, ChevronDown, Menu, LucideMenu} from "lucide-react"
 import { Button } from "@/components/ui/button"
@@ -17,6 +18,82 @@ import {
 } from "@/components/ui/popover"
 import { Switch } from "@/components/ui/switch"
 
+import * as React from "react"
+import { cn } from "@/lib/utils"
+import {
+  NavigationMenu,
+  NavigationMenuContent,
+  NavigationMenuItem,
+  NavigationMenuLink,
+  NavigationMenuList,
+  NavigationMenuTrigger,
+  navigationMenuTriggerStyle,
+} from "@/components/ui/navigation-menu"
+
+const components: { title: string; href: string; description: string }[] = [
+  {
+    title: "What we do",
+    href: "/about#what-we-do",
+    description:
+      "About our company, our products, our services, and our team.",
+  },
+  {
+    title: "Our Story",
+    href: "/about#our-story",
+    description:
+      "How we started and what drives us to create the best experience for our customers.",
+  },
+  {
+    title: "Our Vision",
+    href: "/about#our-vision",
+    description:
+      "Our vision is to be the leading provider of innovative and reliable solutions for our customers.",
+  },
+  {
+    title: "What People say",
+    href: "/about#what-people-say",
+    description: "We have over 1 000 000 players in our community.",
+  },
+  {
+    title: "Cars",
+    href: "/content/cars",
+    description:
+      "In our collection we have over 100 customized and tuned cars!",
+  },
+  {
+    title: "Maps",
+    href: "/content/maps",
+    description:
+      "We selected best maps with customized checkpoints, traffic systems and weather.",
+  },
+]
+
+const ListItem = React.forwardRef<
+  React.ElementRef<"a">,
+  React.ComponentPropsWithoutRef<"a">
+>(({ className, title, children, ...props }, ref) => {
+  return (
+    <li>
+      <NavigationMenuLink asChild>
+        <a
+          ref={ref}
+          className={cn(
+            "block select-none space-y-1 rounded-md p-3 leading-none no-underline outline-none transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground",
+            className
+          )}
+          {...props}
+        >
+          <div className="text-sm font-medium leading-none">{title}</div>
+          <p className="line-clamp-2 text-sm leading-snug text-muted-foreground">
+            {children}
+          </p>
+        </a>
+      </NavigationMenuLink>
+    </li>
+  )
+})
+ListItem.displayName = "ListItem"
+
 
 export function NavBar() {
   return (
@@ -27,18 +104,71 @@ export function NavBar() {
           <img src="/NoHesiLogo.svg" alt="No Hesi" className="h-5" />
           </Link>
           <div className="hidden xl:flex items-center lg-gap-2 xl:gap-4 2xl:gap-6">
-            <Link href="#" className="text-sm text-content-secondary font-bold rounded-sm lg:p-1 xl:p-2 hover:bg-top-light-17 transition-all duration-500">
-              Get started 
-            </Link>
-            <Link href="#" className="text-sm text-content-secondary font-bold rounded-sm lg:p-1 xl:p-2 hover:bg-top-light-17 transition-all duration-500">
-              About Us
-            </Link>
+            <NavigationMenu>
+              <NavigationMenuList>
+                <NavigationMenuItem>
+                  <Link href="/get-started">
+                  <NavigationMenuTrigger className="bg-transparent text-sm text-content-secondary font-bold rounded-sm lg:p-1 xl:p-2 hover:bg-top-light-17 transition-all duration-500">Getting started</NavigationMenuTrigger>
+                  </Link>
+                  <NavigationMenuContent>
+                    <ul className="grid gap-3 p-4 md:w-[400px] lg:w-[500px] lg:grid-cols-[.75fr_1fr]">
+                      <li className="row-span-3">
+                        <NavigationMenuLink asChild className=" bg-cover bg-center " style={{ 
+                              backgroundImage: `linear-gradient(rgba(0, 0, 0, 0.5), rgba(0, 0, 0, 0.5)), url(/discord_banner.webp)` 
+                            }}>
+                          <a
+                            className="flex h-full w-full select-none flex-col justify-center rounded-md p-6 no-underline outline-none focus:shadow-md"
+                            href="/"
+                          >
+                            <img src="/NoHesiLogo.svg" alt="No Hesi" className="h-15" />
+                            <div className="mb-1 mt-4 text-lg font-medium">
+                              No Hesitation
+                            </div>
+                            <p className="text-sm leading-tight text-gray-200">
+                             Cut trough the traffic like a pro!
+                            </p>
+                          </a>
+                        </NavigationMenuLink>
+                      </li>
+                      <ListItem href="/docs" title="Introduction">
+                        Check Requirements, Purchase & Install Assetto Corsa Ultimate Edition
+                      </ListItem>
+                      <ListItem href="/docs/installation" title="Installation">
+                        How to install CM, CSP and other plugins
+                      </ListItem>
+                      <ListItem href="/docs/connecting" title="Accessing Servers">
+                        How to find, filter and connect to the servers
+                      </ListItem>
+                    </ul>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+                <NavigationMenuItem >
+                  <NavigationMenuTrigger className="bg-transparent text-sm text-content-secondary font-bold rounded-sm lg:p-1 xl:p-2 hover:bg-top-light-17 transition-all duration-500">
+                    About Us
+                  </NavigationMenuTrigger>
+                  <NavigationMenuContent>
+                    <ul className="grid w-[400px] gap-3 p-4 md:w-[500px] md:grid-cols-2 lg:w-[600px] ">
+                      {components.map((component) => (
+                        <ListItem
+                          key={component.title}
+                          title={component.title}
+                          href={component.href}
+                        >
+                          {component.description}
+                        </ListItem>
+                      ))}
+                    </ul>
+                  </NavigationMenuContent>
+                </NavigationMenuItem>
+              </NavigationMenuList>
+            </NavigationMenu>
             <Link href="#" className="text-sm text-content-secondary font-bold rounded-sm lg:p-1 xl:p-2 hover:bg-top-light-17 transition-all duration-500">
               Partners
             </Link>
             <Link href="#" className="text-sm text-content-secondary font-bold rounded-sm lg:p-1 xl:p-2 hover:bg-top-light-17 transition-all duration-500">
               Contact
             </Link>
+
           </div>
         </div>
         <div className="xl:hidden flex items-center gap-4">
@@ -95,6 +225,7 @@ export function NavBar() {
               <DropdownMenuItem variant="destructive">Log Out</DropdownMenuItem>
             </DropdownMenuContent>
           </DropdownMenu>
+          
         </div>
         <div className="hidden xl:flex items-center gap-4">
           <div className="flex items-center gap-2 bg-top-light-2 p-1 rounded-md">
